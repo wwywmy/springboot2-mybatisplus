bc21f3fb6cfe3ca6582d4ed65a514de66e23ac17
wwywmy <378297869@qq.com>
2019-10-15 16:47:42 +0800

    增加项目swagger支持

--- src/main/java/com/abc/springboot2/mybatisplus/controller/StudentController.java
+++ src/main/java/com/abc/springboot2/mybatisplus/controller/StudentController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.abc.springboot2.mybatisplus.service.StudentService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping("/student")
+@Api(value = "学生模块")
 public class StudentController {
 
 	@Autowired
 	private StudentService studentService;
 	
+	@ApiOperation(value = "查询用户", notes = "返回所有用户列表")
 	@RequestMapping("/list")
 	public Object selectList() {
 		return studentService.selectList();
